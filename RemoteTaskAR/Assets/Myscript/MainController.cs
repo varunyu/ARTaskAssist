c68ee18fde528da1bd130de05c8a0b428a9a2e73
varunyu <varunyu_f@hotmail.com>
2016-10-24 17:09:05 +0900

    change some image and edit main scene

--- RemoteTaskAR/Assets/Myscript/MainController.cs
+++ RemoteTaskAR/Assets/Myscript/MainController.cs
@@ -15,4 +15,32 @@ public class MainController : MonoBehaviour {
 	public void GotoARGrav(){
 		SceneManager.LoadScene ("AR_Gravity");
 	}
+	public void GotoScenes(int i){
+		switch (i) {
+		case 1:
+			SceneManager.LoadScene ("AR_Normal");
+			break;
+		case 2:
+			SceneManager.LoadScene ("Tutorial_Normal");
+			break;
+		case 3:
+			SceneManager.LoadScene ("EasyTask_Normal");
+			break;
+		case 4:
+			SceneManager.LoadScene ("HardTask_Normal");
+			break;
+		case 5:
+			SceneManager.LoadScene ("AR_Gravity");
+			break;
+		case 6:
+			SceneManager.LoadScene ("Tutorial_Grav");
+			break;
+		case 7:
+			SceneManager.LoadScene ("EasyTask_Grav");
+			break;
+		case 8:
+			SceneManager.LoadScene ("HardTask_Grav");
+			break;
+		}
+	}
 }
